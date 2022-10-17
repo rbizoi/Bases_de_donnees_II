a6d12d26620153850469c3effd9375e4c1ed1afc
rbizoi <razvan.bizoi@gmail.com>
2022-10-17 20:17:20 +0200

    Update 01-les_variables.sql

--- 01-les_variables.sql
+++ 01-les_variables.sql
@@ -1,16 +1,25 @@
 set serveroutput on
 var v_li varchar2(20)
 
+/*
+  Commentaire muti-lignes
+  suite commentaire 
+*/
+-- Commentaire mono-ligne
 declare  
-    v_pl varchar(20) := 'Tintin';   
-    v_utl varchar(20);   
-    v_subst varchar(20) := &variable;
+    v_pl    varchar(20) := 'Tintin';   
+    v_date  date        := sysdate;
+    v_subst varchar(20) := '&variable';
+    v_utl   varchar(20);   
+    v_uid   number      := uid;   
 begin
     :v_li := v_pl;
 	v_utl := user;
-	dbms_output.put_line('v_pl    = '||v_pl);
-	dbms_output.put_line('v_li    = '||:v_li);
+	dbms_output.put_line('v_pl    = '||v_pl   );
+	dbms_output.put_line('v_li    = '||:v_li  );
 	dbms_output.put_line('v_subst = '||v_subst);
-	dbms_output.put_line('v_utl   = '||v_utl );
+	dbms_output.put_line('v_utl   = '||v_utl  );
+    dbms_output.put_line('v_date  = '||v_date );
+    dbms_output.put_line('v_uid   = '||v_uid  );
 end;
 /
